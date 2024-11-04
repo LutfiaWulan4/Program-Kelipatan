2c3fc400b9461b976c7b88f84a4f8f17eae0c372
Lutfia Wulandari <wulanfia4@gmail.com>
2024-11-04 18:51:04 +0700

    Add files via upload

---
+++ Latihan_18.java
@@ -0,0 +1,21 @@
+
+package latihan_pertemuan_6;
+/**
+ * NAMA              : Lutfia Wulandari
+ * KELAS             : PBO12
+ * NIM               : 23176008
+ * PRODI             : Sistem Informasi
+ * SEMESTER          : 3
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * program           : Kelipatan
+ */
+public class Latihan_18 {
+
+    public static void main(String[] args) {
+        double angka = 3.5;
+        while (angka <= 35){
+            System.out.println(angka);
+            angka += 3.5;
+        }
+    }
+}
